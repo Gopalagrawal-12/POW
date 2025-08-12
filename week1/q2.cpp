db4ae80f91f692d1d3a31f9596c198dbaa75629c
Gopalagrawal-12 <gopal.agrawal_cs.aiml24@gla.ac.in>
2025-08-12 22:28:35 +0530

    Update q2.cpp

--- week1/q2.cpp
+++ week1/q2.cpp
@@ -1,1 +1,43 @@
+//Find the Greatest Common Divisor (GCD) of N Numbers
 
+// Approach 1: first find the largest and smallest and then find gcd by running a loop only to smallest integer
+// time complexity : O(n + min element)
+// space complexity : O(1)
+class Solution {
+public:
+    int find(int l,int s){
+        int x=INT_MIN;
+        for(int i=1;i<=s;++i){
+            if(s%i==0 && l%i==0) x=max(x,i);
+        }return x;
+    }
+    int findGCD(vector<int>& nums) {
+        int l=INT_MIN;
+        int s=INT_MAX;
+        for(int i=0;i<nums.size();++i){
+            l=max(l,nums[i]);
+            s=min(s,nums[i]);
+        }
+        return find(l,s);
+    }
+};
+// approach 2 : by euclidean algorithm
+// time complexity : O(n + log(maximum element))
+// space complexity : O(1)
+
+class Solution {
+public:
+    int find(int s,int l){
+        if(s==0) return l;
+        return find(l%s,s);
+    }
+    int findGCD(vector<int>& nums) {
+        int l=INT_MIN;
+        int s=INT_MAX;
+        for(int i=0;i<nums.size();++i){
+            l=max(l,nums[i]);
+            s=min(s,nums[i]);
+        }
+        return find(s,l);
+    }
+};
