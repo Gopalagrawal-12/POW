7afe977cb7a2502078432ce546ee66ff70d1ac35
Gopalagrawal-12 <gopal.agrawal_cs.aiml24@gla.ac.in>
2025-08-29 22:38:47 +0530

    Create q2.java

---
+++ week7/q2.java
@@ -0,0 +1,30 @@
+//approach : first create a constructor then create a function that takes multiple apply() as parameters then by switching a boolean each time
+// time complexity : O(n)
+// space complexity : O(1)
+public class AlternatingSum {
+    private final int v;
+    private final boolean add;
+
+    public AlternatingSum(int v, boolean add) {
+        this.v = v;
+        this.add = add;
+    }
+    public Function<Integer, AlternatingSum> apply() {
+        return (Integer x) -> {
+            if (addNext) {
+                return new AlternatingSum(value + x, false);
+            } else {
+                return new AlternatingSum(value - x, true);
+            }
+        };
+    }
+    public static AlternatingSum add_subtract(int num) {
+        return new AlternatingSum(num, false);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(add_subtract(7)); 
+        System.out.println(add_subtract(1).apply().apply(2).apply(3)); 
+        System.out.println(add_subtract(-5).apply().apply(10).apply(3).apply(9));
+    }
+}
